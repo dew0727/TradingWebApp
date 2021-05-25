f3b2bb104e3b13d65c7d88ad7a107779efebbd43
rockstar <rockstar.space.dev@gmail.com>
2021-05-26 05:23:28 +0800

    Fix currency dropdown menu issue

--- client/src/components/SymbolSelector/index.js
+++ client/src/components/SymbolSelector/index.js
@@ -1,52 +1,42 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Menu, Dropdown, Button } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import { Select } from "antd";
+
+const { Option } = Select;
 
 const SymbolSelector = ({ symbols, callback, defaultIndex, defaultValue }) => {
   const [symbol, setSym] = useState("Select");
 
-  const updateSymbol = useCallback((sym) => {
-    console.log({sym})
-    setSym(sym);
-    if (callback !== undefined) callback(sym);
-  }, [callback]);
+  const updateSymbol = useCallback(
+    (sym) => {
+      console.log({ sym });
+      setSym(sym);
+      if (callback !== undefined) callback(sym);
+    },
+    [callback]
+  );
 
   useEffect(() => {
     if (symbols && symbols.length > defaultIndex && symbol === "Select") {
-      console.log('updating default index')
+      console.log("updating default index");
       updateSymbol(symbols[defaultIndex]);
     }
-  }, [symbols, defaultIndex, symbol, updateSymbol ]);
+  }, [symbols, defaultIndex, symbol, updateSymbol]);
 
   useEffect(() => {
-    if (defaultValue && defaultValue.length > 1)  { 
-      console.log('updating default value')
-      setSym(defaultValue); 
+    if (defaultValue && defaultValue.length > 1) {
+      console.log("updating default value");
+      setSym(defaultValue);
     }
-  }, [defaultValue])
-
-  const menu = (
-    <Menu
-      onClick={(e) => {
-        updateSymbol(e.key);
-      }}
-    >
-      {symbols.map((item, index) => {
-        return (
-          <Menu.Item key={item} danger={item === "Basket"}>
-            {item}
-          </Menu.Item>
-        );
-      })}
-    </Menu>
-  );
+  }, [defaultValue]);
 
   return (
-    <Dropdown overlay={menu} subMenuOpenDelay={0}>
-      <Button>
-        {symbol} <DownOutlined />
-      </Button>
-    </Dropdown>
+    <Select value={symbol} onChange={updateSymbol}>
+      {symbols.map((item, index) => (
+        <Option value={item} key={index}>
+          {item}
+        </Option>
+      ))}
+    </Select>
   );
 };
 
